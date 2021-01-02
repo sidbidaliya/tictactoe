afacc32de7821d80e8682850802476b3e9a05e9c
sid <sidd.bidaliya2000@gmail.com>
2021-01-02 14:03:12 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return <>
+  <h1>Welcome to React Parcel Micro App!</h1>
+  <p>Hard to get more minimal than this React app. Graphic era university</p>
+  <small>hello this is me sidhant bidaliya</small>
+</>
+}
+
+export default App;
